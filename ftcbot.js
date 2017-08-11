2a9c62ca42aa93693b73ed93c79c1157b34dc18f
jmaff <jmaff2003@gmail.com>
2017-08-11 11:32:46 -0400

    FTC Bot 0.1

---
+++ ftcbot.js
@@ -0,0 +1,175 @@
+var env = require('node-env-file');
+env(__dirname + '/.env');
+
+
+if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
+  console.log('Error: Specify clientId clientSecret and PORT in environment');
+  usage_tip();
+  process.exit(1);
+}
+
+var Botkit = require('botkit');
+var debug = require('debug')('botkit:main');
+
+var bot_options = {
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    // debug: true,
+    scopes: ['bot'],
+    studio_token: process.env.studio_token,
+    studio_command_uri: process.env.studio_command_uri
+};
+
+// Use a mongo database if specified, otherwise store in a JSON file local to the app.
+// Mongo is automatically configured when deploying to Heroku
+if (process.env.MONGO_URI) {
+    var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.MONGO_URI});
+    bot_options.storage = mongoStorage;
+} else {
+    bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
+}
+
+// Create the Botkit controller, which controls all instances of the bot.
+var controller = Botkit.slackbot(bot_options);
+
+controller.startTicking();
+
+// Set up an Express-powered webserver to expose oauth and webhook endpoints
+var webserver = require(__dirname + '/components/express_webserver.js')(controller);
+
+// Set up a simple storage backend for keeping a record of customers
+// who sign up for the app via the oauth
+require(__dirname + '/components/user_registration.js')(controller);
+
+// Send an onboarding message when a new team joins
+require(__dirname + '/components/onboarding.js')(controller);
+
+// no longer necessary since slack now supports the always on event bots
+// // Set up a system to manage connections to Slack's RTM api
+// // This will eventually be removed when Slack fixes support for bot presence
+// var rtm_manager = require(__dirname + '/components/rtm_manager.js')(controller);
+//
+// // Reconnect all pre-registered bots
+// rtm_manager.reconnect();
+
+// Enable Dashbot.io plugin
+require(__dirname + '/components/plugin_dashbot.js')(controller);
+
+
+var normalizedPath = require("path").join(__dirname, "skills");
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  require("./skills/" + file)(controller);
+});
+
+var rules = require(__dirname + '/plugins/rules.js');
+
+
+// This captures and evaluates any message sent to the bot as a DM
+// or sent to the bot in the form "@bot message" and passes it to
+// Botkit Studio to evaluate for trigger words and patterns.
+// If a trigger is matched, the conversation will automatically fire!
+// You can tie into the execution of the script using the functions
+// controller.studio.before, controller.studio.after and controller.studio.validate
+if (process.env.studio_token) {
+    //controller.on('direct_message,direct_mention,mention', function(bot, message) {
+        //controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(function(convo) {
+  controller.on('slash_command',function(slashCommand,message) {
+
+  switch(message.command) {
+    case "/echo":
+
+            if (message.text === "" || message.text === "help") {
+                slashCommand.replyPrivate(message,
+                    "I echo back what you tell me. " +
+                    "Try typing `/echo hello` to see.");
+                return;
+            }
+
+            // If we made it here, just echo what the user typed back at them
+            //TODO You do it!
+            slashCommand.replyPublic(message, message.text);
+
+            break;
+      
+    case "/ping":
+      slashCommand.replyPublic(message, "Pong!");
+      
+      break;
+      
+    case "/ftcroot":
+      if (!isNaN(message.text)) {
+          slashCommand.replyPublic(message, 'http://www.ftcroot.com/teams/' + message.text);
+        return;
+          }
+      
+      else {
+        slashCommand.replyPublic(message, message.text + " is not a valid team number.");
+        return;
+      }
+      
+      break;
+      
+    case "/uptime":
+      var uptime = formatUptime(process.uptime())
+      slashCommand.replyPublic(message, uptime);
+      break;
+      
+    case "/rule":
+      var rule = message.text;
+      slashCommand.replyPublic(message, rules.rulebook[rule]);
+      break;
+      
+        default:
+            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+    
+    
+  }
+  
+
+});  
+          
+          
+          
+        //}).catch(function(err) {
+            //bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
+            //debug('Botkit Studio: ', err);
+      
+  
+} else {
+    console.log('~~~~~~~~~~');
+    console.log('NOTE: Botkit Studio functionality has not been enabled');
+    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
+}
+
+
+
+
+function usage_tip() {
+    console.log('~~~~~~~~~~');
+    console.log('Botkit Starter Kit');
+    console.log('Execute your bot application like this:');
+    console.log('clientId=<MY SLACK CLIENT ID> clientSecret=<MY CLIENT SECRET> PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
+    console.log('Get Slack app credentials here: https://api.slack.com/apps')
+    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
+    console.log('~~~~~~~~~~');
+}
+
+function formatUptime(uptime) {
+  var unit = 'second';
+  if (uptime > 60) {
+    uptime = uptime / 60;
+    unit = 'minute';
+  }
+  
+  if (uptime > 60) {
+    uptime = uptime / 60;
+    unit = 'hour';
+  }
+  
+  if (uptime != 1) {
+    unit = unit + 's';
+  }
+  
+  uptime = uptime + ' ' + unit;
+  return uptime;
+}
